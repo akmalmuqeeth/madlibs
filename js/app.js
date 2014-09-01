9971f0d610ddafebe9ccde628bc744720f258f4c
akmal <akmal@brighter.com>
2014-08-31 22:48:40 -0700

    bind to data array

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
 angular.module("madLibApp", []).
 controller("madlibController", function($scope){
-	$scope.maleName ="John"; 
-	$scope.jobTitle ="plumber"; 
-	$scope.tediousTask ="picking trash";
-
-	$scope.dirtyTask ="doing laundry"; 
-	$scope.celebrity ="David Beckham"; 
-	$scope.uselessSkill ="picking trash";
-
-	$scope.obnoCelebrity ="Blasting Bob"; 
-	$scope.adjective ="fastest"; 
-	$scope.hugeNumber ="5674325";
-
+	$scope.data = {maleName: 'John', 
+				   jobTitle:'plumber',
+				   tediousTask: 'picking trash',
+				   dirtyTask: 'doing laundry',
+					celebrity:'David Beckham',
+					uselessSkill:'flying backwards',
+					obnoCelebrity:'Blasting Bob',
+					adjective:'fastest',
+					hugeNumber:'5674325'};
 
 });
